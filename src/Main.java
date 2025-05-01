eb101182e137b3d2e837b10947b9222f52fbec3f
erenvural49 <vuraleren121@gmail.com>
2025-05-01 17:08:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -191,7 +191,7 @@ public class Main extends Application {
         );
         return startButton;
     }
-//w
+
 
     public static void main(String[] args) throws FileNotFoundException{
         readCoordinates("C:\\Users\\erenv\\OneDrive\\Desktop\\TermProject\\levels\\level3.txt");
