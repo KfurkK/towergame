9da8bab63cc649c7c3bb54e3a6e63cbeddbc19ec
erenvural49 <vuraleren121@gmail.com>
2025-05-07 16:29:41 +0300

    fetch yap artık

--- src/Main.java
+++ src/Main.java
@@ -84,7 +84,7 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
         
-        getloseButton().setOnAction(we ->{
+        loseButton.setOnAction(we ->{
         	
         	primaryStage.setScene(scene);
         	
